20e2f17566bb25a27ddae8946cc7770554956dd0
plklc <pllkllczz@gmail.com>
2026-09-07 17:01:21 +1000

    feat: implement screen navigation

--- app/src/main/java/com/example/trueortrue/PlayerSetupActivity.java
+++ app/src/main/java/com/example/trueortrue/PlayerSetupActivity.java
@@ -1,6 +1,9 @@
 package com.example.trueortrue;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayerSetupActivity extends AppCompatActivity {
@@ -8,5 +11,12 @@ public class PlayerSetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_setup);
+
+        Button startGameBtn = findViewById(R.id.startGameBtn);
+
+        startGameBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(PlayerSetupActivity.this, GameActivity.class);
+            startActivity(intent);
+        });
     }
 }
